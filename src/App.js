290bad169dc0ea2eaa8d37a2ef972f604200b651
Hussainsab <echmg1@gmail.com>
2023-11-17 17:18:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { addDoc, collection } from "@firebase/firestore";
+import { firebase } from "./firebase";
 function App() {
+  const [data, setData] = useState("");
+  const handleInput = (e) => {
+    console.log(e.target.value);
+
+    setData(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!data) return;
+    try {
+      const docRef = await addDoc(collection(firebase, "books"), {
+        title: data,
+      });
+      setData("");
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>Book title</label>
+        <input type="text" value={data} onChange={handleInput}></input>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
